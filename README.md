e40a56b121e9259267b340cdb3a09aa33aa5f743
aduner <famin854001260@gmail.com>
2021-01-25 20:08:18 +0800

    Initialize Project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SpringBoot-build-personal-blog
+
 SpringBoot搭建个人博客，入门学习SpringBoot的实战练手项目。
---
+++ src/test/java/com/aduner/SpringbootBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
